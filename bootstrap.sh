e56f456ec7888b9f1b7bddf80e1caaf454e61e2b
Johannes M <johannes@falskdansker.net>
2015-02-22 01:50:41 +0100

    Make bootstrap.sh directory aware

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,59 +1,118 @@
-#!/bin/sh
+#!/bin/bash
 
-# Folders containing the files
-BASE="homedir"
-BASE_SSH="ssh"
-BASE_GPG="gpg"
+# change to the current directory
+REPOSITORY="$( dirname "${BASH_SOURCE[0]}" )"
+# VARIABLES
+## Source folders
+BASE=$REPOSITORY/homedir
+BASE_SSH=$REPOSITORY/ssh
+BASE_GPG=$REPOSITORY/gpg
 
-TARGET="~"
-TARGET_SSH="~/.ssh"
-TARGET_GPG="~/.gnupg"
+## Destination folders
+TARGET=~
+TARGET_SSH=$TARGET/.ssh
+TARGET_GPG=$TARGET/.gnupg
 
+## Application parameters
 RSYNC='rsync --backup --update --itemize-changes --checksum --recursive'
 
+# COPY FILES
 echo "Copying dotfiles.."
 
-$RSYNC $BASE/ $TARGET/
+## Check source permissions
+for i in "$BASE" "$BASE_SSH" "$BASE_GPG" ; do
+	if [ ! -d $i ] && [ ! -r $i ] ; then
+		echo "error: check source permissions ($i)"
+		exit 2
+	fi
+done
 
-# .gitconfig_local
-if [ -r $TARGET/.gitconfig_local ] ; then
-	echo "$TARGET/.gitconfig_local >> $TARGET/.gitconfig"
-	cat $TARGET/.gitconfig_local >> $TARGET/.gitconfig
+
+## Homedir files
+### Check permissions
+if [ ! -d "$TARGET" ] || [ ! -w "$TARGET" ] ; then
+	echo "error: check destination permissions ($TARGET)"
+	exit 1
+fi
+### Copy
+$RSYNC "$BASE/" "$TARGET/"
+
+### Append _local
+#### -> .gitconfig
+if [ -r "$TARGET/.gitconfig_local" ] ; then
+	if [ $(cmp -s "$TARGET/.gitconfig" "$BASE/.gitconfig") ] ; then
+		echo "$TARGET/.gitconfig_local >> $TARGET/.gitconfig"
+		cat "$TARGET/.gitconfig_local" >> "$TARGET/.gitconfig"
+	else
+		echo "$TARGET/.gitconfig has been modified, won't append _local!"
+	fi
+fi
+#### -> .gitignore_global
+if [ -r "$TARGET/.gitignore_global_local" ] ; then
+	if [ $(cmp -s "$TARGET/.gitignore_global" "$BASE/.gitignore_global") ] ; then
+		echo "$TARGET/.gitignore_global_local >> $TARGET/.gitignore_global"
+		cat "$TARGET/.gitignore_global_local" >> "$TARGET/.gitignore_global"
+	else
+		echo "$TARGET/.gitignore_global has been modified, won't append _local!"
+	fi
+fi
+#### -> .vim
+if [ -r "$TARGET/.vimrc_local" ] ; then
+	if [ $(cmp -s "$TARGET/.vimrc" "$BASE/.vimrc") ] ; then
+		echo "$TARGET/.vimrc_local >> $TARGET/.vimrc"
+		cat "$TARGET/.vimrc_local" >> "$TARGET/.vimrc"
+	else
+		echo "$TARGET/.vimrc has been modified, won't append _local!"
+	fi
 fi
 
-# .gitignore_global_local
-if [ -r $TARGET/.gitignore_global_local ] ; then
-	echo "cat $TARGET/.gitignore_global_local >> $TARGET/.gitignore_global"
-	cat $TARGET/.gitignore_global_local >> $TARGET/.gitignore_global
+## SSH / authorized keys
+### Check target permissions
+if [ ! -d "$TARGET_SSH" ] || [ ! -w "$TARGET_SSH" ] ; then
+	mkdir -p "$TARGET_SSH" >/dev/null 2>&1
+	if [ ! -d "$TARGET_SSH" ] || [ ! -w "$TARGET_SSH" ] ; then
+		echo "error: check destination permissions ($TARGET_SSH)"
+		exit 1
+	fi
 fi
 
-#vim_local
-if [ -r $TARGET/.vimrc_local ] ; then
-	echo "cat $TARGET/.vimrc_local >> $TARGET/.vimrc"
-	cat $TARGET/.vimrc_local >> $TARGET/.vimrc
+### Copy files
+$RSYNC "$BASE_SSH/" "$TARGET_SSH/"
+
+#### _local authorized_keys
+if [ -r "$TARGET_SSH/authorized_keys_local" ] ; then
+	if [ $(cmp -s "$TARGET_SSH/authorized_keys" "$BASE_SSH/authorized_keys") ] ; then
+		echo "$TARGET_SSH/authorized_keys_local >> $TARGET_SSH/authorized_keys"
+		cat "$TARGET_SSH/authorized_keys_local" >> "$TARGET_SSH/authorized_keys"
+	else
+		echo "$TARGET_SSH/authorized_keys has been modified, won't append _local!"
+	fi
+fi
+
+## GPG
+### Check target permissions
+
+if [ ! -w "$TARGET_GPG"  ]; then
+	if [ ! -d "$TARGET_GPG" ] ; then
+		mkdir -p "$TARGET_GPG" >/dev/null 2>&1
+	fi
+		echo "error: check destination permissions ($TARGET_GPG)"
+		exit 1
+fi
+
+### Copy files
+$RSYNC "$BASE_GPG/" "$TARGET_GPG/"
+
+### _local gpg.conf
+if [ -r "$TARGET_GPG/gpg.conf_local" ] ; then
+	if [ $(cmp -s "$TARGET_GPG/gpg.conf" "$BASE_GPG/gpg.conf") ] ; then
+		echo "$TARGET_GPG/gpg.conf_local >> $TARGET_GPG/gpg.conf"
+		cat "$TARGET_GPG/gpg.conf_local" >> "$TARGET_GPG/gpg.conf"
+	else
+		echo "$TARGET_GPG/gpg.conf has been modified, won't append _local!"
+	fi
 fi
 
 
-#ssh
-mkdir -p $TARGET_SSH || exit 1
-
-$RSYNC $BASE_SSH/ $TARGET_SSH/ 
-
-if [ -r $TARGET_SSH/authorized_keys_local ] ; then
-	echo "cat $TARGET_SSH/authorized_keys_local >> $TARGET_SSH/authorized_keys"
-	cat $TARGET_SSH/authorized_keys_local >> $TARGET_SSH/authorized_keys
-fi
-
-
-#gpg
-mkdir -p $TARGET_GPG || exit 2
-
-$RSYNC $BASE_GPG/ $TARGET_GPG/
-
-if [ -r $TARGET_GPG/gpg.conf_local ] ; then
-	echo "cat $TARGET_GPG/gpg.conf_local >> $TARGET_GPG/gpg.conf"
-	cat $TARGET_GPG/gpg.conf_local >> $TARGET_GPG/gpg.conf
-fi
-
-
+# That's it!
 echo "Done!"
