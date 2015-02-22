2212f084bd6c5e73e0d1008e23cb67994561119c
Johannes M <johannes@falskdansker.net>
2015-02-23 00:12:35 +0100

    Remove complicated compare commands

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,6 +15,7 @@ TARGET_GPG=$TARGET/.gnupg
 
 ## Application parameters
 RSYNC='rsync --backup --update --itemize-changes --checksum --recursive'
+RSYNC='rsync --backup --itemize-changes --checksum --recursive'
 
 # COPY FILES
 echo "Copying dotfiles.."
@@ -40,30 +41,18 @@ $RSYNC "$BASE/" "$TARGET/"
 ### Append _local
 #### -> .gitconfig
 if [ -r "$TARGET/.gitconfig_local" ] ; then
-	if [ $(cmp -s "$TARGET/.gitconfig" "$BASE/.gitconfig") ] ; then
-		echo "$TARGET/.gitconfig_local >> $TARGET/.gitconfig"
-		cat "$TARGET/.gitconfig_local" >> "$TARGET/.gitconfig"
-	else
-		echo "$TARGET/.gitconfig has been modified, won't append _local!"
-	fi
+	echo "$TARGET/.gitconfig_local >> $TARGET/.gitconfig"
+	cat "$TARGET/.gitconfig_local" >> "$TARGET/.gitconfig"
 fi
 #### -> .gitignore_global
 if [ -r "$TARGET/.gitignore_global_local" ] ; then
-	if [ $(cmp -s "$TARGET/.gitignore_global" "$BASE/.gitignore_global") ] ; then
-		echo "$TARGET/.gitignore_global_local >> $TARGET/.gitignore_global"
-		cat "$TARGET/.gitignore_global_local" >> "$TARGET/.gitignore_global"
-	else
-		echo "$TARGET/.gitignore_global has been modified, won't append _local!"
-	fi
+	echo "$TARGET/.gitignore_global_local >> $TARGET/.gitignore_global"
+	cat "$TARGET/.gitignore_global_local" >> "$TARGET/.gitignore_global"
 fi
 #### -> .vim
 if [ -r "$TARGET/.vimrc_local" ] ; then
-	if [ $(cmp -s "$TARGET/.vimrc" "$BASE/.vimrc") ] ; then
-		echo "$TARGET/.vimrc_local >> $TARGET/.vimrc"
-		cat "$TARGET/.vimrc_local" >> "$TARGET/.vimrc"
-	else
-		echo "$TARGET/.vimrc has been modified, won't append _local!"
-	fi
+	echo "$TARGET/.vimrc_local >> $TARGET/.vimrc"
+	cat "$TARGET/.vimrc_local" >> "$TARGET/.vimrc"
 fi
 
 ## SSH / authorized keys
@@ -81,12 +70,8 @@ $RSYNC "$BASE_SSH/" "$TARGET_SSH/"
 
 #### _local authorized_keys
 if [ -r "$TARGET_SSH/authorized_keys_local" ] ; then
-	if [ $(cmp -s "$TARGET_SSH/authorized_keys" "$BASE_SSH/authorized_keys") ] ; then
-		echo "$TARGET_SSH/authorized_keys_local >> $TARGET_SSH/authorized_keys"
-		cat "$TARGET_SSH/authorized_keys_local" >> "$TARGET_SSH/authorized_keys"
-	else
-		echo "$TARGET_SSH/authorized_keys has been modified, won't append _local!"
-	fi
+	echo "$TARGET_SSH/authorized_keys_local >> $TARGET_SSH/authorized_keys"
+	cat "$TARGET_SSH/authorized_keys_local" >> "$TARGET_SSH/authorized_keys"
 fi
 
 ## GPG
@@ -105,12 +90,8 @@ $RSYNC "$BASE_GPG/" "$TARGET_GPG/"
 
 ### _local gpg.conf
 if [ -r "$TARGET_GPG/gpg.conf_local" ] ; then
-	if [ $(cmp -s "$TARGET_GPG/gpg.conf" "$BASE_GPG/gpg.conf") ] ; then
-		echo "$TARGET_GPG/gpg.conf_local >> $TARGET_GPG/gpg.conf"
-		cat "$TARGET_GPG/gpg.conf_local" >> "$TARGET_GPG/gpg.conf"
-	else
-		echo "$TARGET_GPG/gpg.conf has been modified, won't append _local!"
-	fi
+	echo "$TARGET_GPG/gpg.conf_local >> $TARGET_GPG/gpg.conf"
+	cat "$TARGET_GPG/gpg.conf_local" >> "$TARGET_GPG/gpg.conf"
 fi
 
 
